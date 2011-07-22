7709407b5aaffa7bf0c0ad9a16b13788d1870f05
berni <berni@extensa.pl>
2011-07-22 13:16:09 +0200

    some code styling

--- basics.py
+++ basics.py
@@ -12,7 +12,7 @@ import string, re, StringIO, datetime, random
 # BeautifulSoup, selenium, mysql, crypto, django, gd, scapy
 
 # simple one
-d = {1:2, 4:54, 3:22}
+d = {1: 2, 4: 54, 3: 22}
 for k in d:
     print k
     
@@ -44,8 +44,8 @@ def ff2(number):
     strn = str(number)
     out = []
     i = len(strn)
-    while i>3:
-        out.append(strn[i-3:i])
+    while i > 3:
+        out.append(strn[i - 3:i])
         i -= 3
     out.append(strn[0:i])
     out.reverse()
@@ -53,14 +53,14 @@ def ff2(number):
 print "String ", ",".join(ff2(123456789))
 
 # Create a list by getting rid of elements matching criteria
-a= [1,2,4,2,5,6,7,2,3,4,5,2,4,2,3,6]
+a = [1, 2, 4, 2, 5, 6, 7, 2, 3, 4, 5, 2, 4, 2, 3, 6]
 print [x for x in a if x != 2]
 
 # Get letter and number of repetitions of 
 # letter with max repetitions in a string
 a = "dskjhewuiyerjfdjkghewirfjkfsdkjgfuyweruygsdfhdfsgfsdjhgweaytewuy"
 d = {}
-[d.__setitem__(x,1+d.get(x,0)) for x in list(a)] 
+[d.__setitem__(x, 1 + d.get(x, 0)) for x in list(a)] 
 print "MAX TOUPLE", max([(d[x],x) for x in d])
 
 # What * and ** mean in function declaration
